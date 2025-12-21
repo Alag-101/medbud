9ff64a8d6e35600c96466571bf27c133c2e547f6
Alag-101 <149261717+Alag-101@users.noreply.github.com>
2025-12-21 23:53:02 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,204 @@
+PHARMACY_DATA = [
+
+    # ---------------- DOLO 650 ----------------
+    {
+        "medicine": "Dolo 650",
+        "app": "PharmEasy",
+        "available": True,
+        "distance_km": 1.8,
+        "delivery_minutes": 25,
+        "price": 28,
+        "link": "https://pharmeasy.in"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Apollo Pharmacy",
+        "available": False,
+        "distance_km": 1.2,
+        "delivery_minutes": 20,
+        "price": 27,
+        "link": "https://apollopharmacy.in"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Tata 1mg",
+        "available": True,
+        "distance_km": 3.5,
+        "delivery_minutes": 45,
+        "price": 30,
+        "link": "https://1mg.com"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Netmeds",
+        "available": True,
+        "distance_km": 4.2,
+        "delivery_minutes": 50,
+        "price": 29,
+        "link": "https://netmeds.com"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "MedPlus",
+        "available": False,
+        "distance_km": 2.7,
+        "delivery_minutes": 35,
+        "price": 26,
+        "link": "https://medplusmart.com"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Wellness Forever",
+        "available": True,
+        "distance_km": 3.0,
+        "delivery_minutes": 40,
+        "price": 28,
+        "link": "https://wellnessforever.com"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Medlife",
+        "available": True,
+        "distance_km": 5.0,
+        "delivery_minutes": 60,
+        "price": 29,
+        "link": "#"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "TrustMeds",
+        "available": True,
+        "distance_km": 4.8,
+        "delivery_minutes": 55,
+        "price": 28,
+        "link": "#"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Zeno Health",
+        "available": False,
+        "distance_km": 1.9,
+        "delivery_minutes": 28,
+        "price": 25,
+        "link": "#"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Myra Medicines",
+        "available": True,
+        "distance_km": 6.2,
+        "delivery_minutes": 70,
+        "price": 31,
+        "link": "#"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Local Pharmacy Network",
+        "available": True,
+        "distance_km": 0.9,
+        "delivery_minutes": 18,
+        "price": 32,
+        "link": "#"
+    },
+
+    # ---------------- PARACETAMOL 500 ----------------
+    {
+        "medicine": "Paracetamol 500",
+        "app": "PharmEasy",
+        "available": True,
+        "distance_km": 2.1,
+        "delivery_minutes": 30,
+        "price": 22,
+        "link": "https://pharmeasy.in"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Apollo Pharmacy",
+        "available": True,
+        "distance_km": 1.5,
+        "delivery_minutes": 22,
+        "price": 21,
+        "link": "https://apollopharmacy.in"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Tata 1mg",
+        "available": False,
+        "distance_km": 3.8,
+        "delivery_minutes": 48,
+        "price": 24,
+        "link": "https://1mg.com"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Netmeds",
+        "available": True,
+        "distance_km": 4.0,
+        "delivery_minutes": 50,
+        "price": 23,
+        "link": "https://netmeds.com"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "MedPlus",
+        "available": True,
+        "distance_km": 2.9,
+        "delivery_minutes": 35,
+        "price": 20,
+        "link": "https://medplusmart.com"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Wellness Forever",
+        "available": False,
+        "distance_km": 3.3,
+        "delivery_minutes": 42,
+        "price": 22,
+        "link": "https://wellnessforever.com"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Medlife",
+        "available": True,
+        "distance_km": 5.4,
+        "delivery_minutes": 60,
+        "price": 23,
+        "link": "#"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "TrustMeds",
+        "available": True,
+        "distance_km": 4.6,
+        "delivery_minutes": 55,
+        "price": 22,
+        "link": "#"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Zeno Health",
+        "available": False,
+        "distance_km": 2.0,
+        "delivery_minutes": 30,
+        "price": 19,
+        "link": "#"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Myra Medicines",
+        "available": True,
+        "distance_km": 6.5,
+        "delivery_minutes": 70,
+        "price": 25,
+        "link": "#"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Local Pharmacy Network",
+        "available": True,
+        "distance_km": 1.0,
+        "delivery_minutes": 20,
+        "price": 26,
+        "link": "#"
+    }
+]
