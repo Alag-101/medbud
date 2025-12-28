17a250a2b774c9e8383e7fd2b4f655f88da10fce
Alag-101 <149261717+Alag-101@users.noreply.github.com>
2025-12-28 17:02:47 +0530

    Update data.py 0.1

--- data.py
+++ data.py
@@ -1,10 +1,11 @@
 PHARMACY_DATA = [
 
-    # ---------------- DOLO 650 ----------------
+    # ================= DOLO 650 - HYDERABAD =================
     {
         "medicine": "Dolo 650",
         "app": "PharmEasy",
         "available": True,
+        "location": "Miyapur",
         "distance_km": 1.8,
         "delivery_minutes": 25,
         "price": 28,
@@ -14,6 +15,7 @@ PHARMACY_DATA = [
         "medicine": "Dolo 650",
         "app": "Apollo Pharmacy",
         "available": False,
+        "location": "Miyapur",
         "distance_km": 1.2,
         "delivery_minutes": 20,
         "price": 27,
@@ -23,6 +25,7 @@ PHARMACY_DATA = [
         "medicine": "Dolo 650",
         "app": "Tata 1mg",
         "available": True,
+        "location": "Miyapur",
         "distance_km": 3.5,
         "delivery_minutes": 45,
         "price": 30,
@@ -32,6 +35,7 @@ PHARMACY_DATA = [
         "medicine": "Dolo 650",
         "app": "Netmeds",
         "available": True,
+        "location": "Miyapur",
         "distance_km": 4.2,
         "delivery_minutes": 50,
         "price": 29,
@@ -41,6 +45,7 @@ PHARMACY_DATA = [
         "medicine": "Dolo 650",
         "app": "MedPlus",
         "available": False,
+        "location": "Miyapur",
         "distance_km": 2.7,
         "delivery_minutes": 35,
         "price": 26,
@@ -50,62 +55,61 @@ PHARMACY_DATA = [
         "medicine": "Dolo 650",
         "app": "Wellness Forever",
         "available": True,
+        "location": "Miyapur",
         "distance_km": 3.0,
         "delivery_minutes": 40,
         "price": 28,
         "link": "https://wellnessforever.com"
     },
+
+    # ================= DOLO 650 - BANGALORE =================
+    {
+        "medicine": "Dolo 650",
+        "app": "PharmEasy",
+        "available": True,
+        "location": "Kukatpally",
+        "distance_km": 2.1,
+        "delivery_minutes": 28,
+        "price": 29,
+        "link": "https://pharmeasy.in"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Apollo Pharmacy",
+        "available": True,
+        "location": "Kukatpally",
+        "distance_km": 1.5,
+        "delivery_minutes": 22,
+        "price": 28,
+        "link": "https://apollopharmacy.in"
+    },
+    {
+        "medicine": "Dolo 650",
+        "app": "Tata 1mg",
+        "available": False,
+        "location": "Kukatpally",
+        "distance_km": 3.8,
+        "delivery_minutes": 48,
+        "price": 31,
+        "link": "https://1mg.com"
+    },
     {
         "medicine": "Dolo 650",
         "app": "Medlife",
         "available": True,
-        "distance_km": 5.0,
-        "delivery_minutes": 60,
-        "price": 29,
-        "link": "#"
-    },
-    {
-        "medicine": "Dolo 650",
-        "app": "TrustMeds",
-        "available": True,
-        "distance_km": 4.8,
-        "delivery_minutes": 55,
-        "price": 28,
-        "link": "#"
-    },
-    {
-        "medicine": "Dolo 650",
-        "app": "Zeno Health",
-        "available": False,
-        "distance_km": 1.9,
-        "delivery_minutes": 28,
-        "price": 25,
-        "link": "#"
-    },
-    {
-        "medicine": "Dolo 650",
-        "app": "Myra Medicines",
-        "available": True,
-        "distance_km": 6.2,
-        "delivery_minutes": 70,
-        "price": 31,
-        "link": "#"
-    },
-    {
-        "medicine": "Dolo 650",
-        "app": "Local Pharmacy Network",
-        "available": True,
-        "distance_km": 0.9,
-        "delivery_minutes": 18,
-        "price": 32,
+        "location": "Kukatpally",
+        "distance_km": 2.5,
+        "delivery_minutes": 32,
+        "price": 30,
         "link": "#"
     },
 
-    # ---------------- PARACETAMOL 500 ----------------
+    # ================= PARACETAMOL 500 - HYDERABAD =================
     {
         "medicine": "Paracetamol 500",
         "app": "PharmEasy",
         "available": True,
+        "location": "Miyapur",
         "distance_km": 2.1,
         "delivery_minutes": 30,
         "price": 22,
@@ -115,6 +119,7 @@ PHARMACY_DATA = [
         "medicine": "Paracetamol 500",
         "app": "Apollo Pharmacy",
         "available": True,
+        "location": "Miyapur",
         "distance_km": 1.5,
         "delivery_minutes": 22,
         "price": 21,
@@ -124,6 +129,7 @@ PHARMACY_DATA = [
         "medicine": "Paracetamol 500",
         "app": "Tata 1mg",
         "available": False,
+        "location": "Miyapur",
         "distance_km": 3.8,
         "delivery_minutes": 48,
         "price": 24,
@@ -133,6 +139,7 @@ PHARMACY_DATA = [
         "medicine": "Paracetamol 500",
         "app": "Netmeds",
         "available": True,
+        "location": "Miyapur",
         "distance_km": 4.0,
         "delivery_minutes": 50,
         "price": 23,
@@ -142,63 +149,62 @@ PHARMACY_DATA = [
         "medicine": "Paracetamol 500",
         "app": "MedPlus",
         "available": True,
+        "location": "Miyapur",
         "distance_km": 2.9,
         "delivery_minutes": 35,
         "price": 20,
         "link": "https://medplusmart.com"
     },
+
+    # ================= PARACETAMOL 500 - BANGALORE =================
     {
         "medicine": "Paracetamol 500",
-        "app": "Wellness Forever",
+        "app": "PharmEasy",
+        "available": True,
+        "location": "Kukatpally",
+        "distance_km": 1.8,
+        "delivery_minutes": 25,
+        "price": 23,
+        "link": "https://pharmeasy.in"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Apollo Pharmacy",
         "available": False,
-        "distance_km": 3.3,
-        "delivery_minutes": 42,
+        "location": "Kukatpally",
+        "distance_km": 2.0,
+        "delivery_minutes": 28,
         "price": 22,
-        "link": "https://wellnessforever.com"
+        "link": "https://apollopharmacy.in"
+    },
+    {
+        "medicine": "Paracetamol 500",
+        "app": "Tata 1mg",
+        "available": True,
+        "location": "Kukatpally",
+        "distance_km": 3.2,
+        "delivery_minutes": 42,
+        "price": 25,
+        "link": "https://1mg.com"
     },
     {
         "medicine": "Paracetamol 500",
         "app": "Medlife",
         "available": True,
-        "distance_km": 5.4,
-        "delivery_minutes": 60,
-        "price": 23,
+        "location": "Kukatpally",
+        "distance_km": 2.8,
+        "delivery_minutes": 38,
+        "price": 24,
         "link": "#"
     },
     {
         "medicine": "Paracetamol 500",
-        "app": "TrustMeds",
-        "available": True,
-        "distance_km": 4.6,
-        "delivery_minutes": 55,
-        "price": 22,
-        "link": "#"
-    },
-    {
-        "medicine": "Paracetamol 500",
-        "app": "Zeno Health",
+        "app": "Wellness Forever",
         "available": False,
-        "distance_km": 2.0,
-        "delivery_minutes": 30,
-        "price": 19,
-        "link": "#"
-    },
-    {
-        "medicine": "Paracetamol 500",
-        "app": "Myra Medicines",
-        "available": True,
-        "distance_km": 6.5,
-        "delivery_minutes": 70,
-        "price": 25,
-        "link": "#"
-    },
-    {
-        "medicine": "Paracetamol 500",
-        "app": "Local Pharmacy Network",
-        "available": True,
-        "distance_km": 1.0,
-        "delivery_minutes": 20,
-        "price": 26,
+        "location": "Kukatpally",
+        "distance_km": 3.5,
+        "delivery_minutes": 45,
+        "price": 23,
         "link": "#"
     }
 ]
